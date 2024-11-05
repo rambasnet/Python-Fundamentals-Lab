1abb9aa4d6a79d7763d1bd249af971c5414f793c
Ram Basnet <rambasnet@gmail.com>
2024-11-05 13:20:16 -0700

    update test_file_io.py

--- readonly_labs/fileio/test_file_io.py
+++ readonly_labs/fileio/test_file_io.py
@@ -4,18 +4,22 @@
 import file_io
 
 
-def test_sort_ascending1():
-    my_nums = [10, 9, 0. - 6]
-    file_io.sortListInAscendingOrder(my_nums)
+def test_sort_ascending1() -> None:
+    """Test sort_list_ascending_order function.
+    """
+    my_nums = [10, 9, 0, -6]
+    file_io.sort_list_ascending_order(my_nums)
     assert (my_nums == [-6, 0, 9, 10])
 
 # add 3 more test cases in separate test functions
 
 
-def test_sort_descending1():
+def test_sort_descending1() -> None:
+    """Test sort_list_descending_order function.
+    """
     my_nums = [0, -10, -1, 5, 100]
-    file_io.sortListInDescendingOrder(my_nums)
-    my_nums == [100, 5, 0, -1, -10]
+    file_io.sort_list_descending_order(my_nums)
+    assert my_nums == [100, 5, 0, -1, -10]
 
 
 # add 3 more test cases in seeparate test functions
