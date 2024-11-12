dc16fa086ec3db6c60dc1e0bee40a107644ea5c9
Ram Basnet <rambasnet@gmail.com>
2024-11-12 14:15:39 -0700

    update dicts-morsecodepalindromes

--- readonly_labs/dicts/morsecodepalindromes/test_morsecodepalindromes.py
+++ readonly_labs/dicts/morsecodepalindromes/test_morsecodepalindromes.py
@@ -4,17 +4,22 @@
 import morsecodepalindromes
 
 
-def test_convert_to_palindrome1():
+def test_convert_to_morse() -> None:
+    """Tests convert_to_morse function."""
     actual_ans = morsecodepalindromes.convert_to_morse('AN')
     expected_ans = '.--.'
     assert actual_ans == expected_ans
 
-# FIXME 6 - write 3 more test cases to test convert_to_palindrome function
+# FIXME 6 - write 3 more test funbctions with different cases
+# to test convert_to_morse function
 
 
-def test_ispalindrome1():
+def test_ispalindrome1() -> None:
+    """Tests is_palindrome function.
+    """
     ans = morsecodepalindromes.is_palindrome('.--.')
     expected = 1
     assert ans == expected
 
-# FIXME 7 - Write 3 more test cases to test is_palindrome function
+# FIXME 7 - Write 3 more test functions with different cases
+# to test is_palindrome function
