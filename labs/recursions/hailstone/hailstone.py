f5234fbb74e769cbe371dba142189e26f5d8a575
Ram Basnet <rambasnet@gmail.com>
2025-11-13 10:28:27 -0700

    fix typo hailstone

--- labs/recursions/hailstone/hailstone.py
+++ labs/recursions/hailstone/hailstone.py
@@ -22,7 +22,7 @@ def hailstone_sum(n) -> int:
     """Generate the hailstone sequence starting at n.
 
     The hailstone sequence is defined as follows:
-    - If n is 1, the sume is 1.
+    - If n is 1, the sum is 1.
     - If n is even, the next number is n // 2.
     - If n is odd, the next number is 3 * n + 1.
 
